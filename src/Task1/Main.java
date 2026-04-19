feafccb22a33e3548aa4dddcb29cc14b410e9174
Дарья <@HOME-PC>
2026-04-19 20:32:40 +0300

    Solve problem 1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,5 +1,30 @@
 package Task1;
 
 public class Main {
+    public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m.reverse("J@va the be$t!123"));
+    }
+    String reverse(String str) {
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
 
+        while (left < right) {
+            if (!Character.isLetter(arr[left])) {
+                left++;
+            }
+            else if (!Character.isLetter(arr[right])) {
+                right--;
+            }
+            else {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
 }
